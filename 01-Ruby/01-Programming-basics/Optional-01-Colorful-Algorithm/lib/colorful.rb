c2103286e5ffbbd71a96a06aff38cfd436a89c8f
raphaelcosta <raphaelcosta.dev@gmail.com>
2022-05-05 20:52:55 -0300

    colorfull solved

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,3 +1,30 @@
 def colorful?(number)
-  # TODO: return true if the number is colorful, false otherwise
+  if !number.is_a? Integer
+    return false
+  end
+  array_number = number.digits.reverse
+  final_array = []
+    if array_number.size == 1
+      return true
+    end
+    if array_number.size == 2
+      final_array = array_number
+      final_array << (array_number[0] * array_number[1])
+        if final_array.uniq.length == 3
+          return true
+        else
+          return false
+        end
+    end
+    if array_number.size == 3
+      final_array = array_number
+      final_array << (array_number[0] * array_number[1])
+      final_array << (array_number[1] * array_number[2])
+      final_array << (array_number[0] * array_number[1] * array_number[2])
+        if final_array.uniq.size == 6
+          return true
+        else
+          return false
+        end
+    end
 end
